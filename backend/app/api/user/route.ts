93e4837e75380df86562306b68c7e324eaba1bf8
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-03-14 17:34:51 +0700

    umar: update program

--- backend/app/api/user/route.ts
+++ backend/app/api/user/route.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // buat variabel prisma
 const prisma = new PrismaClient
@@ -34,4 +34,32 @@ export const GET = async () => {
     },{
         status: 200
     })
+}
+
+// buat service "POST" tb_user
+export const POST = async (request:NextRequest) => {
+    //  buat variabel object untuk request
+    const {nama_value, username_value, password_value} = await request.json()
+
+    // proses POST data
+    const save = await prisma.tb_user.create({
+        data: {
+            name: 'nama_value',
+            username: 'username_value',
+            password: 'password_value',
+          }
+    })
+
+    // tampilkan hasil respon
+    return NextResponse.json({
+        meta_data:{
+            error: 0,
+            message: "Data User Berhasil DItambah",
+            status: 201
+        },
+
+    },{
+        status: 201
+    })
+
 }
