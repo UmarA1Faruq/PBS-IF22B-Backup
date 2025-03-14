c942431a2417f788d0e0db1a3c1eae94aef32b5e
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-03-14 16:42:09 +0700

    umar: update program

--- backend/app/api/user/route.ts
+++ backend/app/api/user/route.ts
@@ -8,6 +8,30 @@ const prisma = new PrismaClient
 export const GET = async () => {
     // proses "GET" (tampil data) tb_user
     const data = await prisma.tb_user.findMany({})
+
+    //Kondisi jika data tidak ditemukan
+    if(data.length == 0)
+    {
+        return NextResponse.json({
+            meta_data:{
+                error: 1,
+                message: "Data User Tidak Ditemukan",
+                status: 404
+            },
+        },{
+            status: 404
+        })
+    }
+
     // tampilkan hasil respon
-    return NextResponse.json(data)
+    return NextResponse.json({
+        meta_data:{
+            error: 0,
+            message: null,
+            status: 200
+        },
+        data_user: data
+    },{
+        status: 200
+    })
 }
