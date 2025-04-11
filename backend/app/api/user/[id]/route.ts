f6b11f905051ae4665f2b4b3063e11249a145b36
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-04-11 18:27:14 +0700

    umar: update program

--- backend/app/api/user/[id]/route.ts
+++ backend/app/api/user/[id]/route.ts
@@ -5,7 +5,8 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient
 
 // Buat service "DELETE" (hapus data) tb_user
-export const DELETE = async (request: NextRequest, { params }: { params: { id: string } }) => {
+export const DELETE = async (request: NextRequest, props: { params: Promise<{ id: string }> }) => {
+    const params = await props.params;
     try {
         // cek apakah "id" tersedia/tidak
         const checkId = await prisma.tb_user.findUnique({
@@ -63,3 +64,56 @@ export const DELETE = async (request: NextRequest, { params }: { params: { id: s
         })
     }
 }
+
+
+// buat service "GET" (detail data) tb_user
+export const GET = async (request: NextRequest, props: { params: Promise<{ id: string }> }) => {
+    const params = await props.params;
+    try {
+        // cek apakah "id" tersedia/tidak
+        const checkId = await prisma.tb_user.findUnique({
+            where: {
+                id: Number(params.id),
+            }
+        })
+
+        //Kondisi jika data tidak ditemukan
+        if (!checkId) {
+            return NextResponse.json({
+                meta_data: {
+                    error: 1,
+                    message: "Data User Tidak Ditemukan",
+                    status: 404
+                },
+            }, {
+                status: 404
+            })
+        }
+
+        // tampilkan hasil respon
+        return NextResponse.json({
+            meta_data: {
+                error: 0,
+                message: null,
+                status: 200
+            },
+            data_user: checkId
+        }, {
+            status: 200
+        })
+    }
+    catch (error: any) {
+        // tampilkan hasil respon
+        return NextResponse.json({
+            meta_data: {
+                error: 1,
+                message: "Parameter Slug (ID) Harus Angka!",
+                status: 400
+            },
+
+        }, {
+            status: 400
+        })
+    }
+}
+
