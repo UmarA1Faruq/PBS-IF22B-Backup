46926a086981f06bc47883bd74a94179e6e2fec5
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-04-14 18:22:16 +0700

    umar: update program menambahkan PUT Service

--- backend/app/api/user/[id]/route.ts
+++ backend/app/api/user/[id]/route.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { genSaltSync, hashSync } from "bcrypt-ts";
 
 // buat variabel prisma
 const prisma = new PrismaClient
@@ -117,3 +118,83 @@ export const GET = async (request: NextRequest, props: { params: Promise<{ id: s
     }
 }
 
+// Buat service "PUT" (ubah data) tb_user
+export const PUT = async (request: NextRequest, props: { params: Promise<{ id: string }> }) => {
+    
+    const params = await props.params;
+    // cek apakah "id" tersedia/tidak
+    const checkId = await prisma.tb_user.findUnique({
+        where: {
+            id: Number(params.id),
+        }
+    })
+
+    //Kondisi jika data tidak ditemukan
+    if (!checkId) {
+        return NextResponse.json({
+            meta_data: {
+                error: 1,
+                message: "Data User Tidak Ditemukan",
+                status: 404
+            },
+        }, {
+            status: 404
+        })
+    }
+
+    //  buat variabel object untuk request
+    const {nama_value, username_value, password_value} = await request.json()
+
+    // cek apakah username sudah pernah dibuat / belum
+    const checkUsername = await prisma.tb_user.findMany({
+        where:{
+            username: username_value,
+            NOT:{
+                id: Number(params.id)
+            }
+        }
+    })
+
+    // Jika username ditemukan
+    if(checkUsername.length >=1)
+        {
+            return NextResponse.json({
+                meta_data:{
+                    error: 1,
+                    message: "Data User Gagal Disimpan! Username Sudah Terdaftar!",
+                    status: 404
+                },
+            },{
+                status: 404
+            })
+        }
+
+        // Penambahan bcrypt
+    const password_salt = genSaltSync(10);
+    const password_result = hashSync(password_value, password_salt);
+
+    // proses PUT data
+    const edit = await prisma.tb_user.update({
+        where: {
+            id: Number(params.id),
+        },
+        data: {
+            name: nama_value,
+            username: username_value,
+            password: password_result
+          }
+    })
+
+    // tampilkan hasil respon
+    return NextResponse.json({
+        meta_data:{
+            error: 0,
+            message: "Data User Berhasil Diubah",
+            status: 200
+        },
+
+    },{
+        status: 200
+    })
+
+}
