e7850d3a2a4903fdc02ef6c2499d043ec41daf91
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-05-23 17:50:40 +0700

    umar: membuat layout tambah data

--- frontend/app/add/page.tsx
+++ frontend/app/add/page.tsx
@@ -1,13 +1,29 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function AddPage() {
     return (
-        <div>
+        <div className='bg-amber-300'>
         <fieldset className="fieldset">
             <legend className="fieldset-legend">Nama</legend>
-            <input type="text" className="input" placeholder="Isi Nama User" />
-            <p className="label">Nama User Harus Diisi !</p>
+            <input type="text" className="input w-96" placeholder="Masukan Nama" />
+            <p className="label text-red-700">Nama User Harus Diisi !</p>
         </fieldset>
+
+        <fieldset className="fieldset">
+            <legend className="fieldset-legend">Username</legend>
+            <input type="text" className="input w-96" placeholder="Masukan Usernamer" />
+            <p className="label text-red-700">Username Harus Diisi !</p>
+        </fieldset>
+
+        <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input type="password" className="input w-96" placeholder="Masukan Password" />
+            <p className="label text-red-700">Password Harus Diisi !</p>
+        </fieldset>
+
+        <button className="btn btn-active btn-success ml-5 mr-2.5 mt-5 w-24">Simpan</button>
+        <Link href={"/"} className="btn btn-error ml-2.5 mt-5 w-24">Batal</Link>
         </div>
     )
 }
