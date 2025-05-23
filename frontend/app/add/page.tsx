2038d97a513c1009e70254be5a68e6dbb99a8a7c
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-05-23 18:38:23 +0700

    umar: update

--- frontend/app/add/page.tsx
+++ frontend/app/add/page.tsx
@@ -2,7 +2,7 @@
 "use client"
 
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 export default function AddPage() {
     // buat hook "useState" untuk visible/hidden pesan error
@@ -10,12 +10,32 @@ export default function AddPage() {
     const [errorUsernamaVisible, setErrorUseramaVisible] = useState(false)
     const [errorPasswordVisible, setErrorPasswordVisible] = useState(false)
 
+    // buat hook "useRef" untuk komponen isian
+    const dataNama = useRef<HTMLInputElement>(null)
+
+    // Buat fungsi untuk simpan data
+    const setSaveData = () => {
+        // Cek apakah semuah komponen sudah terisi
+        // jika komponen "nama" tidak diisi
+        if(dataNama.current?.value == "")
+        {
+            // tampilkan pesan error
+            setErrorNamaVisible(true)
+        }
+        // jika komponen "nama" diisi
+        else
+        {
+            // sembunyikan pesan error
+            setErrorNamaVisible(false)
+
+        }
+    }
 
     return (
         <div className='bg-amber-300'>
             <fieldset className="fieldset">
                 <legend className="fieldset-legend">Nama</legend>
-                <input type="text" className="input w-96" placeholder="Masukan Nama" />
+                <input ref={dataNama} type="text" className="input w-96" placeholder="Masukan Nama" />
                 {
                     (errorNamaVisible == true) &&
                     <p className="label text-red-700">Nama User Harus Diisi !</p>
@@ -40,8 +60,13 @@ export default function AddPage() {
                 }
             </fieldset>
 
-            <button className="btn btn-active btn-success ml-5 mr-2.5 mt-5 w-24">Simpan</button>
-            <Link href={"/"} className="btn btn-error ml-2.5 mt-5 w-24">Batal</Link>
+            <button className="btn btn-active btn-success ml-5 mr-2.5 mt-5 w-24" 
+            onClick={setSaveData}>
+            Simpan
+            </button>
+            <Link href={"/"} className="btn btn-error ml-2.5 mt-5 w-24">
+            Batal
+            </Link>
         </div>
     )
 }
