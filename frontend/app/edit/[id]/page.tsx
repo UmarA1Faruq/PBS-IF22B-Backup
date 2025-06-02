3bf885fe1b8cfe6c6d73f0046b2677932f4256b0
UmarA1Faruq <umaralfaruq1424@gmail.com>
2025-06-02 18:34:47 +0700

    umar: tampil data user

--- frontend/app/edit/[id]/page.tsx
+++ frontend/app/edit/[id]/page.tsx
@@ -1,25 +1,46 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
+import axios from 'axios'
 import Link from 'next/link'
-import React, { use, useRef, useState } from 'react'
+import React, { use, useEffect, useRef, useState } from 'react'
 
-export default function EditPage({params}: { params: Promise<{ id: string }> }) {
+export default function EditPage({ params }: { params: Promise<{ id: string }> }) {
     // baca parameter slug "id"
-    const {id} = use(params)
+    const { id } = use(params)
 
     // buat hook "useState" untuk visible/hidden pesan error
-        const [errorNamaVisible, setErrorNamaVisible] = useState(false)
-        const [errorUsernamaVisible, setErrorUsernameVisible] = useState(false)
-        const [errorPasswordVisible, setErrorPasswordVisible] = useState(false)
-    
-        // buat hook "useRef" untuk komponen isian
-        const dataNama = useRef<HTMLInputElement>(null)
-        const dataUsername = useRef<HTMLInputElement>(null)
-        const dataPassword = useRef<HTMLInputElement>(null)
+    const [errorNamaVisible, setErrorNamaVisible] = useState(false)
+    const [errorUsernamaVisible, setErrorUsernameVisible] = useState(false)
+    const [errorPasswordVisible, setErrorPasswordVisible] = useState(false)
+
+    // buat hook "useRef" untuk komponen isian
+    const dataNama = useRef<HTMLInputElement>(null)
+    const dataUsername = useRef<HTMLInputElement>(null)
+    const dataPassword = useRef<HTMLInputElement>(null)
+
+    // buat fungsi untuk "detailData"
+    // ambil service "DetailData"
+    const detailData = async () => {
+        const response = await axios.get(`http://localhost:3001/api/user/${id}`);
+        // jika data user ditemukan
+        if (response.data.meta_data.error == 0) {
+            // tampilkan data user kemasing-masing object
+            dataNama.current!.value = response.data.data_user.name
+            dataUsername.current!.value = response.data.data_user.username
+            dataPassword.current!.value = response.data.data_user.password
+        }
+    };
+
+    // buat hook "useEffect" untuk menjalankan fungsi "detailData"
+    useEffect(() => {
+        // Panggil fungsi "detailData"
+        detailData()
+    }, [])
+
 
     return (
-        <div className='bg-amber-300'>
+        <div className='bg-yellow-300'>
             <fieldset className="fieldset">
                 <legend className="fieldset-legend">Nama</legend>
                 <input ref={dataNama} type="text" className="input w-96" placeholder="Masukan Nama" />
